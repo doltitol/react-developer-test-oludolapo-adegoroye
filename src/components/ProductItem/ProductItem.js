3a0adaf4e2289c843fadd2fbb74005498da11580
Adegoroye Oludolapo <doltil@yahoo.co.uk>
2022-08-30 17:57:24 +0100

    updated product link

--- src/components/ProductItem/ProductItem.js
+++ src/components/ProductItem/ProductItem.js
@@ -10,12 +10,12 @@ export class ProductItem extends PureComponent {
   };
   render() {
     return (
-      <Link to={`/product/${this.props.product.id}`}>
-        <div
-          className='product-item'
-          onMouseOver={() => this.setState({ showAddToCart: true })}
-          onMouseLeave={() => this.setState({ showAddToCart: false })}
-        >
+      <div
+        className='product-item'
+        onMouseOver={() => this.setState({ showAddToCart: true })}
+        onMouseLeave={() => this.setState({ showAddToCart: false })}
+      >
+        <Link to={`/product/${this.props.product.id}`}>
           <div className='product-item-image'>
             <img
               src={this.props.product.gallery[0]}
@@ -28,17 +28,16 @@ export class ProductItem extends PureComponent {
               </div>
             )}
           </div>
-          {this.state.showAddToCart && (
-            <Icons.AddToCart
-              size={80}
-              color='#5ECE7B'
-              className='product-item-addtocart'
-              //onClick={() => {
-              //  navigate();
-              //}}
-            />
-          )}
-
+        </Link>
+        {this.state.showAddToCart && (
+          <Icons.AddToCart
+            size={80}
+            color='#5ECE7B'
+            className='product-item-addtocart'
+            onClick={() => alert('clicked')}
+          />
+        )}
+        <Link to={`/product/${this.props.product.id}`}>
           <p
             className='product-item-name'
             style={{
@@ -47,21 +46,21 @@ export class ProductItem extends PureComponent {
           >
             {this.props.product.brand} - {this.props.product.name}
           </p>
-          {this.props.product.prices
-            .filter((price) => price.currency.symbol === this.props.currency)
-            .map((item) => (
-              <p
-                className='product-item-price'
-                key={item.currency.symbol}
-                style={{
-                  color: this.props.product.inStock ? '#1D1F22' : '#8D8F9A',
-                }}
-              >
-                {item.currency.symbol} {numberCommaFormatter(item.amount)}
-              </p>
-            ))}
-        </div>
-      </Link>
+        </Link>
+        {this.props.product.prices
+          .filter((price) => price.currency.symbol === this.props.currency)
+          .map((item) => (
+            <p
+              className='product-item-price'
+              key={item.currency.symbol}
+              style={{
+                color: this.props.product.inStock ? '#1D1F22' : '#8D8F9A',
+              }}
+            >
+              {item.currency.symbol} {numberCommaFormatter(item.amount)}
+            </p>
+          ))}
+      </div>
     );
   }
 }
